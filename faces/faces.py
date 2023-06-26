ed3cf2af12ad75060eaa3fa30c78b68d8608e990
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2023-06-26 05:10:13 +0000

    Mon, Jun 26, 2023, 6:10 AM +01:00

--- faces/faces.py
+++ faces/faces.py
@@ -1,9 +1,9 @@
 #Main function that converts text to emoticons automatically.
 def convert(this):
+    print(greeting)
     str.replace(":)", "🙂",this)
     str.replace(":(", "🙁",this)
 
 greeting = input("Say hello ")
 
-convert(face)
-print(greeting)
+convert(greeting)
