9cc113ba82e180ae43d9dd7a6fb62fba35730262
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2023-06-26 23:49:10 +0000

    Tue, Jun 27, 2023, 12:49 AM +01:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,8 +1,9 @@
 #Using match to output answer to the great question of life depending on user input.
 def main():
     answer = input(f"What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+    answer.strip().lower()
     match answer:
-        case "42".strip() | "forty-two".strip() | "forty two".strip() | ":
+        case "42" | "forty-two" | "forty two":
             print("Yes")
         case _:
             print("No")
