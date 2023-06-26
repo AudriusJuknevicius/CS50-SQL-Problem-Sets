9a7b1eb842cd8482aaa382310bfc92f753eac0d3
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2023-06-26 05:52:06 +0000

    Mon, Jun 26, 2023, 6:52 AM +01:00

--- playback/playback.py
+++ playback/playback.py
@@ -1,3 +1,3 @@
 #Output the answer with periods for spaces, in turn slowing down the answer.
-course = input("What course is this? ")
-print(course.replace(" ", "..."))
+course = input("What course is this? ").replace(" ", "...")
+print(f{course})
