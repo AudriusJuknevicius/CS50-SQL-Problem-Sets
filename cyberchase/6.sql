c5bad4ca57a20981118d408123b3fe948f4c8e18
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-19 12:02:41 +0000

    Fri, Jan 19, 2024, 12:02 PM +00:00

--- cyberchase/6.sql
+++ cyberchase/6.sql
@@ -1,7 +1,8 @@
 --list the titles of episodes from season 6 (2008) that were released early, in 2007.
 SELECT
-    titles
+    title
 FROM
     "episodes"
 WHERE
-
+"season" = 6 and "air_date" = "2007%"
+;
