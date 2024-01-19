2700e99ed546343637cab903172ebdb3867818af
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-19 12:34:06 +0000

    Fri, Jan 19, 2024, 12:34 PM +00:00

--- cyberchase/9.sql
+++ cyberchase/9.sql
@@ -1,1 +1,7 @@
 --write a query that counts the number of episodes released in Cyberchase’s first 6 years, from 2002 to 2007, inclusive.
+SELECT
+    count("air_date")
+FROM "episodes"
+where
+"air_date" between "2002-01-21" and "2023-12-31"
+;
