d88b2f3540ab35c6245c48314b185c3195cedbb6
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-19 20:57:26 +0000

    Fri, Jan 19, 2024, 8:57 PM +00:00

--- packages/log.sql
+++ packages/log.sql
@@ -5,3 +5,14 @@
 
 -- *** The Forgotten Gift ***
 
+
+-- *** The Lost Letter ***
+-- First Name: Anneke
+-- Address: 900 Somerville Avenue
+-- Sent: Not long ago
+-- Recipient: Varsha
+-- Recipient Address: 2 Finnegan Street, Uptown (potentially wrong?)
+-- Contents: Congratulatory note
+
+-- Task: Check if the letter has arrived at the destination
+
