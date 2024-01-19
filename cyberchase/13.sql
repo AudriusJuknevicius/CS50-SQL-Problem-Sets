07e18e8797a31fa7e7281fa4f0ec7e451863378b
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-19 13:21:18 +0000

    Fri, Jan 19, 2024, 1:21 PM +00:00

--- cyberchase/13.sql
+++ cyberchase/13.sql
@@ -1,12 +1,12 @@
 -- write a SQL query to explore a question of your choice. This query should:
 -- Involve at least one condition, using WHERE with AND or OR
 
---Question: Episodes that have aired during christmas in seasons 1 to 6.
+--Question: Episodes that have aired during christmas between seasons 1 to 6.
 
 SELECT
     title
 FROM "episodes"
 WHERE
-    "air_date" like "____-12-__" and "season" between 1 and 6 
+    "air_date" like "____-12-__" and "season" between 1 and 6
     ;
 
