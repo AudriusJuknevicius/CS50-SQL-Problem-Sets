75f41e63fc10926eed73275efd4438ddfa116d44
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-19 12:16:37 +0000

    Fri, Jan 19, 2024, 12:16 PM +00:00

--- cyberchase/8.sql
+++ cyberchase/8.sql
@@ -1,6 +1,6 @@
 --write a query that counts the number of episodes released in the last 6 years, from 2018 to 2023, inclusive.
 SELECT
-    count(air_date)
+    count("air_date")
 FROM "episodes"
 where
 "air_date" between like "2018%" and like "2023%"
