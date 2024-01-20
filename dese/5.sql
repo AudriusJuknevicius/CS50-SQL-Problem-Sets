75dc05a8da9137283dfe7117bb409767bcb829fb
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-20 22:22:06 +0000

    Sat, Jan 20, 2024, 10:22 PM +00:00

--- dese/5.sql
+++ dese/5.sql
@@ -1,11 +1,10 @@
 SELECT
-"city",
-count("name") as [Number of Schools],
-
+    "city",
+    count("name") as [Number of Schools]
 FROM
 "schools"
 WHERE
-    "type" = "Public School" and "Number of Schools" =< 3
+    "type" = "Public School" and "Number of Schools" <= 3
 GROUP BY city
 ORDER BY "Number of Schools" DESC, "city" ASC
 
