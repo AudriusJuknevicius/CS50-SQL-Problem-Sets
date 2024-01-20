be8965e0c560b018e8fe9f2375ac0c89b57adec2
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-20 21:48:23 +0000

    Sat, Jan 20, 2024, 9:48 PM +00:00

--- dese/4.sql
+++ dese/4.sql
@@ -1,10 +1,17 @@
+-- SELECT
+--     "name",
+--     "City"
+-- FROM
+--     "schools"
+-- HAVING COUNT("school_ID") > 2
+-- ORDER BY COUNT("school_ID")
+-- LIMIT 10
+-- ;
+
 SELECT
-    "name",
-    "City"
+*
 FROM
-    "schools"
-HAVING COUNT("school_ID") > 2
-ORDER BY COUNT("school_ID")
-LIMIT 10
+"schools"
+LIMIT 50
 ;
 
