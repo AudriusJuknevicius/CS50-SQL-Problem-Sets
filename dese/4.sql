b482ed876802ff80f734561d2ecfe032dace36ef
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-20 21:25:47 +0000

    Sat, Jan 20, 2024, 9:25 PM +00:00

--- dese/4.sql
+++ dese/4.sql
@@ -1,17 +1,13 @@
--- SELECT
---     DISTINCT city,
---     COUNT(MAX("name"))
--- FROM
---     "schools"
--- LIMIT 10
---     ;
-
 SELECT
-    count(Distinct city)
+    "city",
+    "name"
+    (SELECT
+    count(Distinct "city")
+    FROM
+    "schools"
+    )
 FROM
     "schools"
-WHERE
-    "
 LIMIT 10
 ;
 
