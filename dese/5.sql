123c3b299dab2c4f1fe266f2cdc84a06576d77cb
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-20 22:29:25 +0000

    Sat, Jan 20, 2024, 10:29 PM +00:00

--- dese/5.sql
+++ dese/5.sql
@@ -4,7 +4,9 @@ SELECT
 FROM
 "schools"
 WHERE
-    "type" = "Public School" and "Number of Schools" <= 3
+    "type" = "Public School"
+HAVING
+"Number of Schools" <= 3
 GROUP BY city
 ORDER BY "Number of Schools" DESC, "city" ASC
 
