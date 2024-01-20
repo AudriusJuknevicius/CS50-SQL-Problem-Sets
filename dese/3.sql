6a3b705fb5af204c04b15d8e17387a1cf352a218
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-20 20:27:57 +0000

    Sat, Jan 20, 2024, 8:27 PM +00:00

--- dese/3.sql
+++ dese/3.sql
@@ -1,8 +1,4 @@
 SELECT
-    pupils,
     AVG("per_pupil_expenditure") as [Average District Per-Pupil Expenditure]
-FROM
-    "districts"
-JOIN "expenditures" on "district_ID" = "districts"."id";
-    ;
-
+FROM "expenditures"
+;
