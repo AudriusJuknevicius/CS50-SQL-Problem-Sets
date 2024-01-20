c89a2784f62abd22305db5975276293dd187dfdd
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-20 22:16:48 +0000

    Sat, Jan 20, 2024, 10:16 PM +00:00

--- dese/4.sql
+++ dese/4.sql
@@ -1,13 +1,3 @@
--- SELECT
---     "name",
---     "City"
--- FROM
---     "schools"
--- HAVING COUNT("school_ID") > 2
--- ORDER BY COUNT("school_ID")
--- LIMIT 10
--- ;
-
 SELECT
 "city",
 count("name") as [Number of Schools]
