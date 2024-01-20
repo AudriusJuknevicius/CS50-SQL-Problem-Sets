13438f9ff4d3c9007c61866273672214bdec5856
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-20 23:06:44 +0000

    Sat, Jan 20, 2024, 11:06 PM +00:00

--- dese/8.sql
+++ dese/8.sql
@@ -5,7 +5,7 @@ SELECT
     e.pupils
 FROM
 "districts" d
-INNER JOIN "schools" s ON d.id. = s.district.id
+INNER JOIN "schools" s ON d.id = s.district_id
 INNER JOIN "expenditures" e on s.district_id = e.district_id
 ORDER BY e.pupils DESC
 )
