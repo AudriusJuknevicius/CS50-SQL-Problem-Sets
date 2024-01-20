029497c343493ae18f610e7cf987f25ea48f6f7d
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-20 22:13:11 +0000

    Sat, Jan 20, 2024, 10:13 PM +00:00

--- dese/4.sql
+++ dese/4.sql
@@ -10,14 +10,14 @@
 
 SELECT
 "city",
-count("name") as q
+count("name") as [Number of Schools]
 
 FROM
 "schools"
 WHERE
     "type" = "Public School"
-GROUP BY "city"
-ORDER BY COUNT("q"), "city" DESC
+GROUP BY city
+ORDER BY "Number of Schools" DESC, "city" ASC
 LIMIT 10
 ;
 
