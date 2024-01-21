2d58baed3198cf3a5a555d39dc9def2001914ab1
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-21 13:03:11 +0000

    Sun, Jan 21, 2024, 1:03 PM +00:00

--- dese/12.sql
+++ dese/12.sql
@@ -1,3 +1,8 @@
+-- A parent asks you for advice on finding the best public school districts in Massachusetts.
+-- In 12.sql, write a SQL query to find public school districts with above-average per-pupil expenditures and an above-average percentage of teachers rated “exemplary”.
+-- Your query should return the districts’ names, along with their per-pupil expenditures and percentage of teachers rated exemplary.
+-- Sort the results first by the percentage of teachers rated exemplary (high to low), then by the per-pupil expenditure (high to low).
+
 SELECT
     DISTINCT dis.name,
     exp.per_pupil_expenditure,
