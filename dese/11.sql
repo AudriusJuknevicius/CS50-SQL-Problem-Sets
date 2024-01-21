d669987d6afcce584cc9e2c34c6301e416333db3
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-21 12:09:37 +0000

    Sun, Jan 21, 2024, 12:09 PM +00:00

--- dese/11.sql
+++ dese/11.sql
@@ -1,11 +1,22 @@
+-- SELECT
+--     *
+-- FROM
+--     "districts" as dis
+-- INNER JOIN "schools" as sch ON dis.id = sch.id
+-- INNER JOIN "graduation_rates" as gra ON sch.id = gra.id
+-- INNER JOIN "expenditures" as exp ON dis.id = exp.district_id
+-- INNER JOIN "staff_evaluations" as sta ON sta.id = dis.id
+
+-- LIMIT 50
+-- ;
 SELECT
     *
 FROM
     "districts" as dis
-INNER JOIN "schools" as sch ON dis.id = sch.id
-INNER JOIN "graduation_rates" as gra ON sch.id = gra.id
-INNER JOIN "expenditures" as exp ON dis.id = exp.district_id
-INNER JOIN "staff_evaluations" as sta ON sta.id = dis.id
+INNER JOIN "schools" as sch ON exp.district_id = sch.district_id
+INNER JOIN "graduation_rates" as gra ON sch.id = gra.school_id
+INNER JOIN "expenditures" as exp ON sch.district_id = exp.district_id
+INNER JOIN "staff_evaluations" as sta ON exp.district_id = sta.district_id
 
 LIMIT 50
 ;
