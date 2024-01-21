a47eb9a0335287c59b04437764445e3cd49280b4
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-21 12:12:41 +0000

    Sun, Jan 21, 2024, 12:12 PM +00:00

--- dese/11.sql
+++ dese/11.sql
@@ -13,7 +13,7 @@ SELECT
     *
 FROM
     "districts" as dis
-INNER JOIN "schools" as sch ON exp.district_id = sch.district_id
+INNER JOIN "schools" as sch ON dis.id = sch.district_id
 INNER JOIN "graduation_rates" as gra ON sch.id = gra.school_id
 INNER JOIN "expenditures" as exp ON sch.district_id = exp.district_id
 INNER JOIN "staff_evaluations" as sta ON exp.district_id = sta.district_id
