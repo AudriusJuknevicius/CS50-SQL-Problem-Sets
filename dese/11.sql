f463c8adb21d8f033561b789d8ad37c501e38a00
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-21 11:49:17 +0000

    Sun, Jan 21, 2024, 11:49 AM +00:00

--- dese/11.sql
+++ dese/11.sql
@@ -4,9 +4,9 @@ SELECT
 FROM
     "districts" as d
 INNER JOIN
+    "graduation_rates" as g ON
+INNER JOIN
     "expenditures" as e ON d.id = e.district_id
-WHERE
-    "d"."type" like "%Public%"
 ORDER BY "e"."per_pupil_expenditure" DESC
 LIMIT 10
 ;
