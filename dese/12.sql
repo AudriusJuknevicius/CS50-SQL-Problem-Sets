fbc758f20f297b7994bf8f7a264cc73285de2a61
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-21 12:35:42 +0000

    Sun, Jan 21, 2024, 12:35 PM +00:00

--- dese/12.sql
+++ dese/12.sql
@@ -8,5 +8,7 @@ INNER JOIN "staff_evaluations" as sta ON dis.id = sta.district_id
 INNER JOIN "expenditures" as exp ON dis.id = exp.district_id
 INNER JOIN "schools" as sch ON dis.id = sch.district_id
 INNER JOIN "graduation_rates" as gra ON sch.id = gra.school_id
+WHERE
+    sch.type = "Public School"
 ORDER BY exp.per_pupil_expenditure DESC, sch.name ASC
 ;
