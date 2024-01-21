b81b9125b6b445d88c9966c6e1f25bb906fc8978
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-21 11:51:34 +0000

    Sun, Jan 21, 2024, 11:51 AM +00:00

--- dese/11.sql
+++ dese/11.sql
@@ -2,11 +2,12 @@ SELECT
     "d"."name",
     "e"."per_pupil_expenditure"
 FROM
-    "districts" as d
-INNER JOIN
-    "graduation_rates" as g ON
-INNER JOIN
-    "expenditures" as e ON d.id = e.district_id
+    "districts" as dis
+INNER JOIN "schools" as sch
+INNER JOIN "graduation_rates" as gra ON
+INNER JOIN "expenditures" as exp ON d.id = e.district_id
+INNER JOIN "staff_evaluations" as sta
+
 ORDER BY "e"."per_pupil_expenditure" DESC
 LIMIT 10
 ;
