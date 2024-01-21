a4265954788b6a3493f39f9fcac87e979ffd8fcc
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-21 12:45:25 +0000

    Sun, Jan 21, 2024, 12:45 PM +00:00

--- dese/12.sql
+++ dese/12.sql
@@ -1,27 +1,23 @@
--- SELECT
---     dis.name,
---     exp.per_pupil_expenditure,
---     sta.
--- FROM
---     "districts" as dis
--- INNER JOIN "staff_evaluations" as sta ON dis.id = sta.district_id
--- INNER JOIN "expenditures" as exp ON dis.id = exp.district_id
--- INNER JOIN "schools" as sch ON dis.id = sch.district_id
--- INNER JOIN "graduation_rates" as gra ON sch.id = gra.school_id
--- WHERE
---     sch.type = "Public School"
--- ORDER BY exp.per_pupil_expenditure DESC, sch.name ASC
--- ;
-
 SELECT
-*
+    dis.name,
+    exp.per_pupil_expenditure,
+    sta.exemplary
 FROM
-"staff_evaluations"
+    "districts" as dis
+INNER JOIN "staff_evaluations" as sta ON dis.id = sta.district_id
+INNER JOIN "expenditures" as exp ON dis.id = exp.district_id
+INNER JOIN "schools" as sch ON dis.id = sch.district_id
+INNER JOIN "graduation_rates" as gra ON sch.id = gra.school_id
+WHERE
+    sch.type = "Public School" and
+    exp.per_pupil_expenditure > (
+        SELECT AVG("exp".per_pupil_expenditure
+        )
+    )
+LIMIT 50
 ;
 
 
+-- COLUMNS:
 
-
-COLUMNS:
-
-PUBLIC SCHOOL DISTRICT / Above Average Per Pupil Expenditures / Above Average Percentage of Teachers rated exemplary
+-- PUBLIC SCHOOL DISTRICT / Above Average Per Pupil Expenditures / Above Average Percentage of Teachers rated exemplary
