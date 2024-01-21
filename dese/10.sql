d5ae676ebc15c09ba80e061c263e8a39fc6edacc
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-21 11:41:12 +0000

    Sun, Jan 21, 2024, 11:41 AM +00:00

--- dese/10.sql
+++ dese/10.sql
@@ -4,7 +4,7 @@ SELECT
 FROM
     "districts" as d
 INNER JOIN
-    "expenditures" as e ON d.id = e.district_Id
+    "expenditures" as e ON d.id = e.district_id
 WHERE
     "d"."type" = "Public School"
 ORDER BY "e"."per_pupil_expenditure" DESC
