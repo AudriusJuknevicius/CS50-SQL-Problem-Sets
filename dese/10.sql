0fe056e58e8e52c4fc8fed91252bf80580a32046
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-21 11:40:35 +0000

    Sun, Jan 21, 2024, 11:40 AM +00:00

--- dese/10.sql
+++ dese/10.sql
@@ -1,12 +1,12 @@
 SELECT
-    d.name,
-    "e.per_pupil_expenditure"
+    "d"."name",
+    "e"."per_pupil_expenditure"
 FROM
     "districts" as d
 INNER JOIN
     "expenditures" as e ON d.id = e.district_Id
 WHERE
-    d.type = "Public School"
-ORDER BY e.per_pupil_expenditure DESC
+    "d"."type" = "Public School"
+ORDER BY "e"."per_pupil_expenditure" DESC
 LIMIT 20
 ;
