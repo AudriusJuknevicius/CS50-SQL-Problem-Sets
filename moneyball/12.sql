7e19a8b17c3192409b009a539fb0fcfa1ab93865
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-22 20:54:27 +0000

    Mon, Jan 22, 2024, 8:54 PM +00:00

--- moneyball/12.sql
+++ moneyball/12.sql
@@ -21,7 +21,8 @@ JOIN "performances" as per on per.player_id = pla.id
 WHERE
     per.year = sal.year and per.year = 2001 and per.RBI > 0 and per.H > 0
 ORDER BY
-    sal.salary / per.h ASC,
-    sal.salary / per.RBI ASC
+    pla.id ASC,
+    sal.salary / per.h ASC
+    sal.salary / per.RBI ASC,
 LIMIT 6
 ;
