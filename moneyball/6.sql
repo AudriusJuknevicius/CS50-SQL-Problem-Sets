86381240f58c2cda56ee9e49c2c71d829aee8bad
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-22 17:40:09 +0000

    Mon, Jan 22, 2024, 5:40 PM +00:00

--- moneyball/6.sql
+++ moneyball/6.sql
@@ -14,5 +14,6 @@ INNER JOIN "performances" as per on per.player_id = pla.id
 INNER JOIN "teams" AS tea on tea.id = per.team_id
 -- INNER JOIN "salaries" as sal on sal.player_id = pla.id
 WHERE
-pla.first_name = "Satchel" and pla.last_name = "Paige"
+    per.year = 2001
+ORDER BY per.H DESC
 ;
