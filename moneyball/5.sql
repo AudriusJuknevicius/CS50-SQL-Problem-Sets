4c29dc2886edba9a6d68c6ee32d0890b47231305
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-22 17:25:00 +0000

    Mon, Jan 22, 2024, 5:25 PM +00:00

--- moneyball/5.sql
+++ moneyball/5.sql
@@ -2,15 +2,16 @@
 -- Though Satchel no longer plays, in 5.sql, write a SQL query to find all teams that Satchel Paige played for.
 -- Your query should return a table with a single column, one for the name of the teams.
 
--- SELECT
---     DISTINCT tea.name
--- FROM
---     "players" AS pla
--- INNER JOIN "salaries" as sal on sal.player_id = pla.id
--- INNER JOIN "teams" AS tea on tea.id = sal.team_id
--- WHERE
--- pla.first_name = "Satchel" and pla.last_name = "Paige"
--- ;
+SELECT
+    DISTINCT tea.name
+FROM
+    "players" AS pla
+INNER JOIN "performances" as per on per.player_id = pla.id
+INNER JOIN "teams" AS tea on tea.id = per.team_id
+INNER JOIN "salaries" as sal on sal.player_id = pla.id
+WHERE
+pla.first_name = "Satchel" and pla.last_name = "Paige"
+;
 
 -- SELECT
 --     *
@@ -21,10 +22,10 @@
 -- ;
 
 
-SELECT
-    *
-FROM
-    "performances"
-WHERE
-"player_id" = 14190
-;
+-- SELECT
+--     *
+-- FROM
+--     "performances"
+-- WHERE
+-- "player_id" = 14190
+-- ;
