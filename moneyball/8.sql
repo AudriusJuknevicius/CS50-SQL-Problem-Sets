4605c17201f9d4db1e20e2a45f4738c6b7c0f238
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-22 17:47:49 +0000

    Mon, Jan 22, 2024, 5:47 PM +00:00

---
+++ moneyball/8.sql
@@ -0,0 +1,16 @@
+-- How much would the A’s need to pay to get the best home run hitter this past season?
+-- In 8.sql, write a SQL query to find the 2001 salary of the player who hit the most home runs in 2001.
+-- Your query should return a table with one column, the salary of the player.
+
+SELECT
+    sal.salary
+FROM
+    "players" AS pla
+INNER JOIN "performances" as per on per.player_id = pla.id
+INNER JOIN "teams" AS tea on tea.id = per.team_id
+INNER JOIN "salaries" as sal on sal.player_id = pla.id
+WHERE
+per.year = 2001
+ORDER BY per.HR DESC
+LIMIT 1
+;
