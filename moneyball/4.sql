6637d21f80a66b3d6d019eafaaa8ef73dc102563
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-22 16:52:11 +0000

    Mon, Jan 22, 2024, 4:52 PM +00:00

--- moneyball/4.sql
+++ moneyball/4.sql
@@ -1,3 +1,4 @@
-You need to make a recommendation about which players the team should consider hiring.
-With the team’s dwindling budget, the general manager wants to know which players were paid the lowest salaries in 2001.
-In 4.sql, write a SQL query to find the 50 players paid the least in 2001.
+-- You need to make a recommendation about which players the team should consider hiring.
+-- With the team’s dwindling budget, the general manager wants to know which players were paid the lowest salaries in 2001.
+-- In 4.sql, write a SQL query to find the 50 players paid the least in 2001.
+
