8d1482f389422750a30def0898ddf891edd1ed31
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-23 11:30:51 +0000

    Tue, Jan 23, 2024, 11:30 AM +00:00

--- atl/schema.sql
+++ atl/schema.sql
@@ -3,12 +3,14 @@ CREATE TABLE "passengers" (
     "first_name" TEXT NOT NULL,
     "last_name" TEXT NOT NULL,
     "age" INTEGER NOT NULL
-
+    PRIMARY KEY ("id")
 );
 
 CREATE TABLE "check-ins" (
     "id" INTEGER,
     "passenger_id" INTEGER,
-    "first_name
-    "
+    "datetime" NUMERIC NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "flight_id" INTEGER
+    PRIMARY KEY("id"),
+    FOREIGN KEY
 );
