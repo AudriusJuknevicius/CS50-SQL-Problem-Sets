95885cdcfeceb5d6c290f33eec3906283a41ff54
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-23 12:26:10 +0000

    Tue, Jan 23, 2024, 12:26 PM +00:00

--- connect/schema.sql
+++ connect/schema.sql
@@ -1,18 +1,18 @@
-CREATE TABLE "users" (
-    "id" INTEGER,
-    "first_name" TEXT NOT NULL,
-    "last_name" TEXT NOT NULL,
-    "username" TEXT NOT NULL UNIQUE,
-    "password" TEXT NOT NULL,
-    PRIMARY KEY("id")
-);
+-- CREATE TABLE "users" (
+--     "id" INTEGER,
+--     "first_name" TEXT NOT NULL,
+--     "last_name" TEXT NOT NULL,
+--     "username" TEXT NOT NULL UNIQUE,
+--     "password" TEXT NOT NULL,
+--     PRIMARY KEY("id")
+-- );
 
-CREATE TABLE "schools and universities" (
+CREATE TABLE "schools_and_universities" (
     "id" INTEGER,
     "school_pupil" TEXT NOT NULL,
     "pupil_affiliation_start" TEXT NOT NULL,
     "pupil_affiliation_end" TEXT,
-    "pupil_degree_type" TEXT CHECK('BA','BSc','MA','PhD')
+    "pupil_degree_type" TEXT CHECK('BA', 'BSc', 'MA', 'PhD'),
     "school_name" TEXT NOT NULL,
     "school_type" TEXT NOT NULL,
     "school_location" TEXT NOT NULL,
@@ -21,18 +21,18 @@ CREATE TABLE "schools and universities" (
     FOREIGN KEY("school_pupil") REFERENCES "users"("id")
 );
 
-CREATE TABLE "companies" (
-    "id" INTEGER,
-    "company_employee" TEXT NOT NULL,
-    "employee_start_date" TEXT NOT NULL,
-    "employee_finish_date" TEXT,
-    "employee_title" TEXT NOT NULL,
-    "company_name" TEXT NOT NULL,
-    "company_industry" TEXT NOT NULL,
-    "company_location" TEXT NOT NULL,
-    PRIMARY KEY("id")
-    FOREIGN KEY("company_employee") REFERENCES "users"("id")
-);
+-- CREATE TABLE "companies" (
+--     "id" INTEGER,
+--     "company_employee" TEXT NOT NULL,
+--     "employee_start_date" TEXT NOT NULL,
+--     "employee_finish_date" TEXT,
+--     "employee_title" TEXT NOT NULL,
+--     "company_name" TEXT NOT NULL,
+--     "company_industry" TEXT NOT NULL,
+--     "company_location" TEXT NOT NULL,
+--     PRIMARY KEY("id"),
+--     FOREIGN KEY("company_employee") REFERENCES "users"("id")
+-- );
 
 
 
