cca359d5c762b149ba0f8bb1a8ce6bd3691b5ef2
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-24 07:22:21 +0000

    Wed, Jan 24, 2024, 7:22 AM +00:00

--- donuts/schema.sql
+++ donuts/schema.sql
@@ -3,9 +3,25 @@ ingredients
 ingredient flour, yeast, oil, butter, sugar
 weight grams
 price per unit
+Donut_ingredients
 
 DONUTS
 
 Donut Name
 Donut_Gluten
 Donut_price
+
+
+ORDERS
+
+Order Number
+Donut Order
+Customer
+
+
+Customers
+
+First_name
+last_name
+order_history
+
