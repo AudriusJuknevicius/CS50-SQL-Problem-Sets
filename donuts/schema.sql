3cd164f6a7cd5a889c08b22c5ac16ba868b12533
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-24 07:42:14 +0000

    Wed, Jan 24, 2024, 7:42 AM +00:00

--- donuts/schema.sql
+++ donuts/schema.sql
@@ -1,16 +1,16 @@
-CREATE TABLE "ingredients" (
-    "id" INTEGER,
-    "ingredient" TEXT NOT NULL,
-    "weight" INTEGER NOT NULL,
-    "price_per_unit" NUMERIC NOT NULL,
-    "gluten_check" TEXT NOT NULL CHECK("gluten_check" in ('Gluten', 'Gluten-Free')),
-    PRIMARY KEY("id")
-);
+-- CREATE TABLE "ingredients" (
+--     "id" INTEGER,
+--     "ingredient" TEXT NOT NULL,
+--     "weight" INTEGER NOT NULL,
+--     "price_per_unit" NUMERIC NOT NULL,
+--     "gluten_check" TEXT NOT NULL CHECK("gluten_check" in ('Gluten', 'Gluten-Free')),
+--     PRIMARY KEY("id")
+-- );
 
 CREATE TABLE "donuts" (
     "id" INTEGER,
     "donut_name" TEXT NOT NULL,
-    "donut_gluten" TEXT NOT NULL CHECK("donut_gluten" in ('Gluten', 'Gluten-Free'))
+    "donut_gluten" TEXT NOT NULL CHECK("donut_gluten" in ('Gluten', 'Gluten-Free')),
     "donut_price" NUMERIC,
     "donut_ingredients" TEXT NOT NULL,
     PRIMARY KEY("id"),
@@ -29,13 +29,13 @@ CREATE TABLE "orders" (
     FOREIGN KEY("donut_name") REFERENCES "donuts"("id")
 );
 
-CREATE TABLE "customers" (
-    "id" INTEGER,
-    "first_name" TEXT NOT NULL,
-    "last_name" TEXT NOT NULL,
-    "order_history" TEXT NOT NULL UNIQUE,
-    PRIMARY KEY("id"),
-    FOREIGN KEY("order_history") REFERENCES "orders"("id")
-);
+-- CREATE TABLE "customers" (
+--     "id" INTEGER,
+--     "first_name" TEXT NOT NULL,
+--     "last_name" TEXT NOT NULL,
+--     "order_history" TEXT NOT NULL UNIQUE,
+--     PRIMARY KEY("id"),
+--     FOREIGN KEY("order_history") REFERENCES "orders"("id")
+-- );
 
 
