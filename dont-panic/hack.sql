cccbe855e9e05fdbaf938bc2b9bc7d7625eddbdc
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-27 01:23:38 +0000

    Sat, Jan 27, 2024, 1:23 AM +00:00

--- dont-panic/hack.sql
+++ dont-panic/hack.sql
@@ -1,11 +1,13 @@
-UPDATE "user_logs"
-SET "new_password" = (
-    SELECT "password"
-    FROM "users"
-    WHERE "username" = "admin"
-)
-WHERE "user_logs" = (
-    SELECT "new_password"
-    FROM "user_logs"
-    WHERE "new_username" = "emily33"
-)
+-- UPDATE "user_logs"
+-- SET "new_password" = (
+--     SELECT "password"
+--     FROM "users"
+--     WHERE "username" = "admin"
+-- )
+-- WHERE "user_logs" = (
+--     SELECT "new_password"
+--     FROM "user_logs"
+--     WHERE "new_username" = "emily33"
+-- )
+
+
