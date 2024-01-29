e570682f5686bcd1c0dd8978d8da3febe58d9bfc
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-29 16:21:01 +0000

    Mon, Jan 29, 2024, 4:21 PM +00:00

--- meteorites/import.sql
+++ meteorites/import.sql
@@ -1,62 +1,56 @@
 -- Importing the data into a temporary folder in order to clean it before importing it into a normal table.
 
--- .import --csv meteorites.csv temp1
-
-
--- CREATE TABLE "temp2" (
---     "id" INTEGER,
---     "name" TEXT NOT NULL,
---     "class" TEXT,
---     "mass" NUMERIC,
---     "discovery" TEXT NOT NULL,
---     "year"
--- )
-
+.import --csv meteorites.csv temp1
 
 
 --  Deleting rows with "relict" meteor nametype.
 
--- DELETE FROM "temp1"
--- WHERE "nametype" = "Relict"
--- ;
+DELETE FROM "temp1"
+WHERE "nametype" = "Relict"
+;
 
 -- Updating the three numeric numbers to be rounded to the nearest hundreth.
 
--- UPDATE "temp1"
--- SET "mass" = ROUND("mass", 2),
---     "lat" = ROUND("lat", 2),
---     "long" = ROUND("long", 2);
+UPDATE "temp1"
+SET "mass" = ROUND("mass", 2),
+    "lat" = ROUND("lat", 2),
+    "long" = ROUND("long", 2);
 
 -- Updating zero values to be displayed as "NULL".
 
--- UPDATE "temp1"
--- SET
--- "mass" = CASE WHEN "mass" = 0.0 or 0.00 THEN "NULL" ELSE "mass" END,
--- "lat" = CASE WHEN "lat" = 0.0 or 0.00 THEN "NULL" ELSE "lat" END,
--- "long" = CASE WHEN "long" = 0.0 or 0.00 THEN "NULL" ELSE "long" END
--- "year" = CASE WHEN "year" = "" THEN "NULL" ELSE "year" END
--- ;
-
-
--- CREATE TABLE "meteorites" (
---     "id" INTEGER PRIMARY KEY,
---     "name" TEXT NOT NULL,
---     "class" TEXT,
---     "mass" NUMERIC,
---     "discovery" TEXT,
---     "year" TEXT,
---     "lat" NUMERIC,
---     "long" NUMERIC
--- );
-
--- INSERT INTO "meteorites" ("name", "class", "mass", "discovery", "year", "lat", "long")
--- SELECT "name", "class", "mass", "discovery", "year", "lat", "long" FROM "temp1"
--- ORDER BY "year" ASC, "name" ASC
--- ;
-
-SELECT
-    *
-FROM
-    "meteorites"
-LIMIT 200
+UPDATE "temp1"
+SET
+"mass" = CASE WHEN "mass" = 0.0 or 0.00 THEN "NULL" ELSE "mass" END,
+"lat" = CASE WHEN "lat" = 0.0 or 0.00 THEN "NULL" ELSE "lat" END,
+"long" = CASE WHEN "long" = 0.0 or 0.00 THEN "NULL" ELSE "long" END
+"year" = CASE WHEN "year" = "" THEN "NULL" ELSE "year" END
 ;
+
+
+-- Creating the table with the right data types and constraints.
+
+CREATE TABLE "meteorites" (
+    "id" INTEGER PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "class" TEXT,
+    "mass" NUMERIC,
+    "discovery" TEXT,
+    "year" TEXT,
+    "lat" NUMERIC,
+    "long" NUMERIC
+);
+
+
+-- Moving the cleaned data minus the ID number, letting Sqlite make new IDs dependant on the order by statement.
+
+INSERT INTO "meteorites" ("name", "class", "mass", "discovery", "year", "lat", "long")
+SELECT "name", "class", "mass", "discovery", "year", "lat", "long" FROM "temp1"
+ORDER BY "year" ASC, "name" ASC
+;
+
+-- SELECT
+--     *
+-- FROM
+--     "meteorites"
+-- LIMIT 200
+-- ;
