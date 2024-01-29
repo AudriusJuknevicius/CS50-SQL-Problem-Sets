fe067a91177753c2f31223573132771f2d2cf7c3
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-29 16:02:50 +0000

    Mon, Jan 29, 2024, 4:02 PM +00:00

--- meteorites/import.sql
+++ meteorites/import.sql
@@ -14,19 +14,29 @@
 
 
 
+--  Deleting rows with "relict" meteor nametype.
 
 -- DELETE FROM "temp1"
 -- WHERE "nametype" = "Relict"
 -- ;
+
+-- Updating the three numeric numbers to be rounded to the nearest hundreth.
 
 -- UPDATE "temp1"
 -- SET "mass" = ROUND("mass", 2),
 --     "lat" = ROUND("lat", 2),
 --     "long" = ROUND("long", 2);
 
+-- Updating zero values to be displayed as "NULL".
+
+-- UPDATE "temp1"
+-- SET
+-- "mass" = CASE WHEN "mass" = 0.0 or 0.00 THEN "NULL" ELSE "mass" END,
+-- "lat" = CASE WHEN "lat" = 0.0 or 0.00 THEN "NULL" ELSE "lat" END,
+-- "long" = CASE WHEN "long" = 0.0 or 0.00 THEN "NULL" ELSE "long" END
+-- ;
+
 UPDATE "temp1"
 SET
-"mass" = CASE WHEN "mass" = 0.0 or 0.00 THEN "NULL" ELSE "mass" END,
-"lat" = CASE WHEN "lat" = 0.0 or 0.00 THEN "NULL" ELSE "lat" END,
-"long" = CASE WHEN "long" = 0.0 or 0.00 THEN "NULL" ELSE "long" END
+"year" = CASE WHEN "year" = "" THEN "NULL" ELSE "year" END
 ;
