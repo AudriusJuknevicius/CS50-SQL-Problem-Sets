7e8427759d9d6176c1b273ebebfccc677494f7ab
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-29 15:57:34 +0000

    Mon, Jan 29, 2024, 3:57 PM +00:00

--- meteorites/import.sql
+++ meteorites/import.sql
@@ -25,6 +25,9 @@
 --     "long" = ROUND("long", 2);
 
 UPDATE "temp1"
-SET "mass" = ROUND("mass", 2),
-    "lat" = ROUND("lat", 2),
-    "long" = ROUND("long", 2);
+SET
+"mass" = CASE WHEN "mass" = 0.0 or 0.00 THEN "NULL" ELSE "mass" END,
+"mass" = CASE WHEN "mass" = 0.0 or 0.00 THEN "NULL" ELSE "mass" END,
+"mass" = CASE WHEN "mass" = 0.0 or 0.00 THEN "NULL" ELSE "mass" END,
+"lat" = ROUND("lat", 2),
+"long" = ROUND("long", 2);
