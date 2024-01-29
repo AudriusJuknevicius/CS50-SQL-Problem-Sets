e5c76c4950ba4585d182b2f321bab73d3969ff2c
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-29 16:43:02 +0000

    Mon, Jan 29, 2024, 4:43 PM +00:00

--- meteorites/import.sql
+++ meteorites/import.sql
@@ -1,62 +1,75 @@
--- Importing the data into a temporary folder in order to clean it before importing it into a normal table.
+-- -- Importing the data into a temporary folder in order to clean it before importing it into a normal table.
 
-CREATE TABLE "temp1" (
-    "name" TEXT,
-    "id" TEXT,
-    "nametype" TEXT,
-    "class" TEXT,
-    "mass" TEXT,
-    "discovery" TEXT,
-    "year" TEXT,
-    "lat" TEXT,
-    "long" TEXT
-);
+-- CREATE TABLE "temp1" (
+--     "name" TEXT,
+--     "id" TEXT,
+--     "nametype" TEXT,
+--     "class" TEXT,
+--     "mass" TEXT,
+--     "discovery" TEXT,
+--     "year" TEXT,
+--     "lat" TEXT,
+--     "long" TEXT
+-- );
 
-.import --csv meteorites.csv --skip 1 temp1
-
-
---  Deleting rows with "relict" meteor nametype.
-
-DELETE FROM "temp1"
-WHERE "nametype" = "Relict"
-;
-
--- Updating the three numeric numbers to be rounded to the nearest hundreth.
-
-UPDATE "temp1"
-SET "mass" = ROUND("mass", 2),
-    "lat" = ROUND("lat", 2),
-    "long" = ROUND("long", 2);
-
--- Updating zero values to be displayed as "NULL".
-
-UPDATE "temp1"
-SET
-"mass" = CASE WHEN "mass" = 0.0 or 0.00 THEN "NULL" ELSE "mass" END,
-"lat" = CASE WHEN "lat" = 0.0 or 0.00 THEN "NULL" ELSE "lat" END,
-"long" = CASE WHEN "long" = 0.0 or 0.00 THEN "NULL" ELSE "long" END,
-"year" = CASE WHEN "year" = "" THEN "NULL" ELSE "year" END
-;
+-- .import --csv meteorites.csv --skip 1 temp1
 
 
--- Creating the table with the right data types and constraints.
+-- --  Deleting rows with "relict" meteor nametype.
 
-CREATE TABLE "meteorites" (
-    "id" INTEGER PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "nametype" TEXT,
-    "class" TEXT,
-    "mass" NUMERIC,
-    "discovery" TEXT,
-    "year" TEXT,
-    "lat" NUMERIC,
-    "long" NUMERIC
-);
+-- DELETE FROM "temp1"
+-- WHERE "nametype" = "Relict"
+-- ;
+
+-- -- Updating the three numeric numbers to be rounded to the nearest hundreth.
+
+-- UPDATE "temp1"
+-- SET "mass" = ROUND("mass", 2),
+--     "lat" = ROUND("lat", 2),
+--     "long" = ROUND("long", 2);
+
+-- -- Updating zero values to be displayed as "NULL".
+
+-- UPDATE "temp1"
+-- SET
+-- "mass" = CASE WHEN "mass" = 0.0 or 0.00 THEN "NULL" ELSE "mass" END,
+-- "lat" = CASE WHEN "lat" = 0.0 or 0.00 THEN "NULL" ELSE "lat" END,
+-- "long" = CASE WHEN "long" = 0.0 or 0.00 THEN "NULL" ELSE "long" END,
+-- "year" = CASE WHEN "year" = "" THEN "NULL" ELSE "year" END
+-- ;
 
 
--- Moving the cleaned data minus the ID number, letting Sqlite make new IDs dependant on the order by statement.
+-- -- Creating the table with the right data types and constraints.
 
-INSERT INTO "meteorites" ("name", "nametype", "class", "mass", "discovery", "year", "lat", "long")
-SELECT "name", "nametype", "class", "mass", "discovery", "year", "lat", "long" FROM "temp1"
-ORDER BY "year" ASC, "name" ASC
-;
+-- CREATE TABLE "meteorites" (
+--     "id" INTEGER PRIMARY KEY,
+--     "name" TEXT NOT NULL,
+--     "nametype" TEXT,
+--     "class" TEXT,
+--     "mass" NUMERIC,
+--     "discovery" TEXT,
+--     "year" TEXT,
+--     "lat" NUMERIC,
+--     "long" NUMERIC
+-- );
+
+
+-- -- Moving the cleaned data minus the ID number, letting Sqlite make new IDs dependant on the order by statement.
+
+-- INSERT INTO "meteorites" ("name", "nametype", "class", "mass", "discovery", "year", "lat", "long")
+-- SELECT "name", "nametype", "class", "mass", "discovery", "year", "lat", "long" FROM "temp1"
+-- ORDER BY "year" ASC, "name" ASC
+-- ;
+
+
+-- SELECT
+--     count(*)
+--     FROM(
+--     "meteorites"
+--     );
+
+-- SELECT
+--     count(*)
+--     FROM (
+--     "temp1"
+--     );
