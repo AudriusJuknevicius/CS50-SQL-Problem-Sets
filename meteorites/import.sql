ff087e911c32c22f1e2ad689748d7795cdaf2ad6
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-29 15:43:42 +0000

    Mon, Jan 29, 2024, 3:43 PM +00:00

--- meteorites/import.sql
+++ meteorites/import.sql
@@ -1,6 +1,6 @@
+-- Importing the data into a temporary folder in order to clean it before importing it into a normal table.
+
 -- .import --csv meteorites.csv temp1
-
-
 
 
 -- CREATE TABLE "temp2" (
@@ -14,11 +14,13 @@
 
 
 
+
 -- DELETE FROM "temp1"
 -- WHERE "nametype" = "Relict"
 -- ;
 
-UPDATE "temp1"
-SET "mass" = ROUND("mass", 2),
-    "lat" = ROUND("lat", 2),
-    "long" = ROUND("long", 2);
+-- UPDATE "temp1"
+-- SET "mass" = ROUND("mass", 2),
+--     "lat" = ROUND("lat", 2),
+--     "long" = ROUND("long", 2);
+
