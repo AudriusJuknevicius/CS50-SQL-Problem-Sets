f85f9f68aa4e9bab2090fbe2883888033e1d6d86
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-01-29 12:13:58 +0000

    Mon, Jan 29, 2024, 12:13 PM +00:00

--- dont-panic/hack.sql
+++ dont-panic/hack.sql
@@ -15,4 +15,9 @@ UPDATE "users" SET "password" = "982c0381c279d139fd221fce974916e7"
 WHERE "username" = "admin"
 ;
 
+DELETE FROM "user_logs"
+WHERE "new_username" = "admin"
+;
+
+INSERT INTO "user_logs" 
 
