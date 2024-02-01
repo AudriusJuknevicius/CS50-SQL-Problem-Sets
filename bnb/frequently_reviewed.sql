754c68c55935e38e192cf122cfc79bbf5cd3d216
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-02-01 08:40:07 +0000

    Thu, Feb 1, 2024, 8:40 AM +00:00

--- bnb/frequently_reviewed.sql
+++ bnb/frequently_reviewed.sql
@@ -9,7 +9,7 @@ SELECT
 FROM "listings" as l
 JOIN "reviews" as r ON l.id = r.listing_id
 GROUP BY r.comments
-ORDER BY r.comments DESC
+ORDER BY r.comments DESC, l.property_type ASC, l.host_name ASC
 LIMIT 100
 ;
 
