59940f0c82191bd166ce4af34ff3b69283205aa4
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-02-01 08:54:06 +0000

    Thu, Feb 1, 2024, 8:54 AM +00:00

--- bnb/june_vacancies.sql
+++ bnb/june_vacancies.sql
@@ -1,2 +1,14 @@
 -- https://cs50.harvard.edu/sql/2024/psets/4/bnb/
 
+SELECT
+    l.id,
+    l.property_type,
+    l.host_name,
+    COUNT(a.listing_id) as days_vacant
+FROM
+    "listings" as l
+JOIN "availabilities" as a on l.id = a.listing_id
+WHERE
+    a.available = TRUE and date = between  "2023-06-01"  "2023-06-30"
+GROUP BY l.id
+;
