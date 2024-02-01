f21e8871ec899d2a7f0ef36bae990fa525941d58
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-02-01 08:09:15 +0000

    Thu, Feb 1, 2024, 8:09 AM +00:00

--- bnb/one_bedrooms.sql
+++ bnb/one_bedrooms.sql
@@ -1,5 +1,13 @@
 -- https://cs50.harvard.edu/sql/2024/psets/4/bnb/
 
 CREATE VIEW "one_bedrooms" AS
-
-
+SELECT
+    "id",
+    "property_type",
+    "host_name",
+    "accommodates"
+FROM
+    "listings"
+WHERE
+    "bedrooms" = 1
+;
