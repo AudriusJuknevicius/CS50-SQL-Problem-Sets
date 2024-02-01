72e39d06bb82b40452a64dedf3ee4bbb79ce5bac
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-02-01 08:31:12 +0000

    Thu, Feb 1, 2024, 8:31 AM +00:00

--- bnb/frequently_reviewed.sql
+++ bnb/frequently_reviewed.sql
@@ -1,2 +1,14 @@
 -- https://cs50.harvard.edu/sql/2024/psets/4/bnb/
 
+CREATE VIEW "frequently_reviewed" AS
+SELECT
+    l.id,
+    l.property_type,
+    l.host_name,
+    r.
+FROM "listings" as l
+JOIN "availabilities" as a ON l.id = a.listing_id
+WHERE
+    a.available = "TRUE"
+;
+
