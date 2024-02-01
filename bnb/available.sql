4e9b41e05d30607bc7f1448192beec232eed500e
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-02-01 08:16:21 +0000

    Thu, Feb 1, 2024, 8:16 AM +00:00

--- bnb/available.sql
+++ bnb/available.sql
@@ -1,1 +1,12 @@
 -- https://cs50.harvard.edu/sql/2024/psets/4/bnb/
+
+-- CREATE VIEW "available" AS
+
+SELECT
+    l.id,
+    l.property_type,
+    l.host_name,
+    a.date
+FROM "listings" as l
+JOIN "availabilities" as a ON l.id = a.listing_id
+;
