dce7a1af3a6ab0d5738ad50e04d11ffa43bba9ec
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-02-01 08:46:23 +0000

    Thu, Feb 1, 2024, 8:46 AM +00:00

--- bnb/frequently_reviewed.sql
+++ bnb/frequently_reviewed.sql
@@ -1,15 +1,15 @@
 -- https://cs50.harvard.edu/sql/2024/psets/4/bnb/
 
--- CREATE VIEW "frequently_reviewed" AS
+CREATE VIEW "frequently_reviewed" AS
 SELECT
     l.id,
     l.property_type,
     l.host_name,
-    COUNT(r.comments) as reviews
+    COUNT(r.id) as reviews
 FROM "listings" as l
 JOIN "reviews" as r ON l.id = r.listing_id
 GROUP BY l.id
-ORDER BY r.comments DESC, l.property_type ASC, l.host_name ASC
+ORDER BY reviews DESC, l.property_type ASC, l.host_name ASC
 LIMIT 100
 ;
 
