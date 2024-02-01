8bf0d04760cebd5b1d0c9e2ed880d79a183380e4
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-02-01 08:06:59 +0000

    Thu, Feb 1, 2024, 8:06 AM +00:00

--- bnb/one_bedrooms.sql
+++ bnb/one_bedrooms.sql
@@ -1,4 +1,5 @@
 -- https://cs50.harvard.edu/sql/2024/psets/4/bnb/
 
-CREATE VIEW "one_bedrooms"
-wadsqqqaszwqa3szeaqw2zsedxqawszaqwsd    `QQAWSEDXCWES34DRFCTAQZWSEDXRCFTVGYBWSEDX|
+CREATE VIEW "one_bedrooms" AS
+
+
