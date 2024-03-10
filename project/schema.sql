975b5c47d0d86753c5606a4f4737dbb769d91c05
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-03-10 16:51:04 +0000

    Sun, Mar 10, 2024, 4:51 PM +00:00

--- project/schema.sql
+++ project/schema.sql
@@ -2,76 +2,72 @@
 
 
 -- Represent users in the music database.
-CREATE TABLE "users" (
-    "id" INT AUTO_INCREMENT,
-    "first_name" VARCHAR(24) NOT NULL,
-    "last_name" VARCHAR(24) NOT NULL,
-    "username" VARCHAR(24) NOT NULL UNIQUE,
-    "display_name" VARCHAR(24) NOT NULL,
-    "email" VARCHAR(24) NOT NULL UNIQUE,
-    "Password" VARCHAR(24) NOT NULL,
-    "registration_date" DATE,
-    "date_of_birth" DATE,
-    "profile_image" BLOB, 
-    PRIMARY KEY("id")
+CREATE TABLE `users` (
+    `id` INT AUTO_INCREMENT,
+    `first_name` VARCHAR(24) NOT NULL,
+    `last_name` VARCHAR(24) NOT NULL,
+    `username` VARCHAR(24) NOT NULL UNIQUE,
+    `display_name` VARCHAR(24) NOT NULL,
+    `email` VARCHAR(24) NOT NULL UNIQUE,
+    `Password` VARCHAR(24) NOT NULL,
+    `registration_date` DATE,
+    `date_of_birth` DATE,
+    `profile_image` BLOB,
+    PRIMARY KEY(`id`)
 );
 
 -- Represent problems in the course
-CREATE TABLE "user_tracks" (
-    "id" INTEGER,
-    "problem_set" INTEGER NOT NULL,
-    "name" TEXT NOT NULL,
-    PRIMARY KEY("id")
+CREATE TABLE `user_tracks` (
+    `id` INTEGER,
+    `problem_set` INTEGER NOT NULL,
+    `name` TEXT NOT NULL,
+    PRIMARY KEY(`id`)
 );
 
-CREATE TABLE "tracks" (
-    "id" INTEGER,
-    "first_name" TEXT NOT NULL,
-    "last_name" TEXT NOT NULL,
-    PRIMARY KEY("id")
+CREATE TABLE `tracks` (
+    `id` INTEGER,
+    `first_name` TEXT NOT NULL,
+    `last_name` TEXT NOT NULL,
+    PRIMARY KEY(`id`)
 );
 
 -- Represent instructors in the course
-CREATE TABLE "playlists" (
-    "id" INTEGER,
-    "first_name" TEXT NOT NULL,
-    "last_name" TEXT NOT NULL,
-    PRIMARY KEY("id")
+CREATE TABLE `playlists` (
+    `id` INTEGER,
+    `first_name` TEXT NOT NULL,
+    `last_name` TEXT NOT NULL,
+    PRIMARY KEY(`id`)
 );
 
 -- Represent submissions of problems by students
-CREATE TABLE "artists" (
-    "id" INTEGER,
-    "student_id" INTEGER,
-    "problem_id" INTEGER,
-    "submission_path" TEXT NOT NULL,
-    "correctness" NUMERIC NOT NULL CHECK("correctness" BETWEEN 0 AND 1),
-    "timestamp" NUMERIC NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY("id"),
-    FOREIGN KEY("student_id") REFERENCES "students"("id"),
-    FOREIGN KEY("problem_id") REFERENCES "problems"("id")
+CREATE TABLE `artists` (
+    `id` INTEGER,
+    `student_id` INTEGER,
+    `problem_id` INTEGER,
+    `submission_path` TEXT NOT NULL,
+    `correctness` NUMERIC NOT NULL CHECK(`correctness` BETWEEN 0 AND 1),
+    `timestamp` NUMERIC NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(`id`),
+    FOREIGN KEY(`student_id`) REFERENCES `students`(`id`),
+    FOREIGN KEY(`problem_id`) REFERENCES `problems`(`id`)
 );
 
 -- Represent individual comments left by instructors
-CREATE TABLE "albums" (
-    "id" INTEGER,
-    "instructor_id" INTEGER,
-    "submission_id" INTEGER,
-    "contents" TEXT NOT NULL,
-    PRIMARY KEY("id"),
-    FOREIGN KEY("submission_id") REFERENCES "submissions"("id")
-    FOREIGN KEY("instructor_id") REFERENCES "instructors"("id")
+CREATE TABLE `albums` (
+    `id` INTEGER,
+    `instructor_id` INTEGER,
+    `submission_id` INTEGER,
+    `contents` TEXT NOT NULL,
+    PRIMARY KEY(`id`),
+    FOREIGN KEY(`submission_id`) REFERENCES `submissions`(`id`)
+    FOREIGN KEY(`instructor_id`) REFERENCES `instructors`(`id`)
 );
 
 
 -- Create indexes to speed common searches
-CREATE INDEX "student_name_search" ON "students" ("first_name", "last_name");
-CREATE INDEX "student_github_search" ON "students" ("github_username");
-CREATE INDEX "problem_name_search" ON "problems" ("name");
+CREATE INDEX `student_name_search` ON `students` (`first_name`, `last_name`);
+CREATE INDEX `student_github_search` ON `students` (`github_username`);
+CREATE INDEX `problem_name_search` ON `problems` (`name`);
 
 _______________________________________________________________________________________________
 
-
-CREATE TABLE "users" (
-    "id" INTEGER,
-)
