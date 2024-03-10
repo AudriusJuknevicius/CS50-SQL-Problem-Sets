b95176a30fd56907d130ab8457c52beaac2421fd
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-03-10 15:36:15 +0000

    Sun, Mar 10, 2024, 3:36 PM +00:00

--- project/schema.sql
+++ project/schema.sql
@@ -1,7 +1,7 @@
 -- In this SQL file, write (and comment!) the schema of your database, including the CREATE TABLE, CREATE INDEX, CREATE VIEW, etc. statements that compose it
 
 
-CREATE TABLE "students" (
+CREATE TABLE "USERS" (
     "id" INTEGER,
     "first_name" TEXT NOT NULL,
     "last_name" TEXT NOT NULL,
