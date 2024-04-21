dac425b61dd9fe3871d7dc8d9527de11e2683f6d
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-04-21 15:04:52 +0000

    Sun, Apr 21, 2024, 4:04 PM +01:00

--- project/queries.sql
+++ project/queries.sql
@@ -2,12 +2,27 @@
 -- In this SQL file, write (and comment!) the typical SQL queries users will run on your database
 
 
+-- Returns each playlist's name along with the total number of tracks in each, sorted
+-- in a descending order by the number of tracks.
 SELECT
     p.playlist_name,
     COUNT(pt.track_id) AS total_tracks
-FROM playlists as p
-JOIN playlist_tracks as pt ON p.playlist_id = pt.playlist_id
+FROM playlists AS p
+JOIN playlist_tracks AS pt ON p.playlist_id = pt.playlist_id
 GROUP BY p.playlist_id, p.playlist_name
 ORDER BY total_tracks DESC
 ;
+
+-- Find top 5 users who have the most tracks.
+SELECT
+    u.id,
+    display_name,
+    COUNT(ut.track_id) AS total_tracks_added
+FROM users AS u
+JOIN user_tracks AS UT ON u.id = ut.user_id
+GROUP BY u.id
+ORDER BY total_tracks_added DESC
+LIMIT 5
+;
+
 
